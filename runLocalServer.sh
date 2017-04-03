d489416c53cbe20c8f664b7eb5028900414afff9
Manfred Moser <manfred@simpligility.com>
2017-04-03 16:55:05 -0700

    new file to serve slides that can use various tools

---
+++ runLocalServer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Starting webserver on port 9000 to allow markdown includes."
+if [ -x "$(command -v python)" ]; then
+  echo " Using python"
+  python -m SimpleHTTPServer 9000
+elif [ -x "$(command -v python3)" ]; then
+  echo " Using python"
+  python3 -m http.server 9000
+elif [ -x "$(command -v ruby)" ]; then
+  echo " Using ruby"
+  ruby -run -ehttpd . -p9000
+elfi [ -x "$(command -v ruby)" ]; then
+  echo " Using busybox"
+  busybox httpd -v -p 9000 -h .
+else 
+  echo "Failed to find python, python3, ruby or busybox - no webserver started."
+fi
+
+
