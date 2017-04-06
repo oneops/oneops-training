b67c52f8443ae8ce0cc4cf42289b6cb0ea4eb1ca
Manfred Moser <manfred@simpligility.com>
2017-04-06 10:05:17 -0700

    added optional port parameter

--- runLocalServer.sh
+++ runLocalServer.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-echo "Starting webserver on port 9000 to allow markdown includes."
+PORT=${1:-9000}
+
+
+echo "Starting webserver on port $PORT to allow markdown includes."
 if [ -x "$(command -v python)" ]; then
   echo " Using python"
-  python -m SimpleHTTPServer 9000
+  python -m SimpleHTTPServer $PORT
 elif [ -x "$(command -v python3)" ]; then
   echo " Using python"
-  python3 -m http.server 9000
+  python3 -m http.server $PORT
 elif [ -x "$(command -v ruby)" ]; then
   echo " Using ruby"
-  ruby -run -ehttpd . -p9000
+  ruby -run -ehttpd . -p$PORT
 elif [ -x "$(command -v busybox)" ]; then
   echo " Using busybox"
-  busybox httpd -v -p 9000 -h .
+  busybox httpd -v -p $PORT -h .
 else
   echo "Failed to find python, python3, ruby or busybox - no webserver started."
 fi
